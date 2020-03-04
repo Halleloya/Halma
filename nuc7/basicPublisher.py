7c515ae7a9c33ca98a2d215cd0b9c6421b6d9809
Halleloya <haoluoyao@163.com>
2020-03-04 14:38:54 -0500

    update publisher

--- nuc7/basicPublisher.py
+++ nuc7/basicPublisher.py
@@ -4,32 +4,49 @@ This is the Publisher
 
 #!/usr/bin/env python3
 
+import sys
 import paho.mqtt.client as mqtt
 import time
 import json
 
 def on_publish(client, userdata, mid):
     print ("publish callback!")
-    pass
+    #pass
+
 def on_disconnect(client, userdata, rc):
     print ("disconnect callback!")
 
-client = mqtt.Client()
-client.connect("111.111.111.1", 1883, 60)
 
-client.on_publish = on_publish
-client.on_disconnect = on_disconnect
+def main():
 
-msg = {"pin":17, "value":0}
-ret = client.publish("topic/gpio", json.dumps(msg), qos=2, retain=False);
-print (ret)
+    client = mqtt.Client()
+    client.connect("111.111.111.1", 1883, 60)
+    client.loop_start()
+    client.on_publish = on_publish
+    client.on_disconnect = on_disconnect
 
-#ret.wait_for_publish():
+    value = int(sys.argv[1])
+    msg = {"pin":17, "value":value}
+    ret = client.publish("topic/gpio", json.dumps(msg), qos=2, retain=False);
+    print (ret)
 
-if ret.is_published():
-    print ("is published")
+    #ret.wait_for_publish():
 
-try:
-    client.loop_forever()
-except KeyboardInterrupt:
+    if ret.is_published():
+        print ("is published")
+    
+    '''
+    try:
+        client.loop()
+    except KeyboardInterrupt:
+        client.disconnect()
+    '''
+    client.loop_stop()
     client.disconnect()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print ('input 0 to turn off, 1 to turn on.')
+        sys.exit()
+    
+    main()
