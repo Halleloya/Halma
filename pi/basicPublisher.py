dd1968c5d5e065e46ec751cf1236e9a22ee0a5da
Halleloya <haoluoyao@163.com>
2020-03-04 15:14:02 -0500

    update pi

---
+++ pi/basicPublisher.py
@@ -0,0 +1,52 @@
+'''
+This is the Publisher
+'''
+
+#!/usr/bin/env python3
+
+import sys
+import paho.mqtt.client as mqtt
+import time
+import json
+
+def on_publish(client, userdata, mid):
+    print ("publish callback!")
+    #pass
+
+def on_disconnect(client, userdata, rc):
+    print ("disconnect callback!")
+
+
+def main():
+
+    client = mqtt.Client()
+    client.connect("111.111.111.2", 1883, 60)
+    client.loop_start()
+    client.on_publish = on_publish
+    client.on_disconnect = on_disconnect
+
+    value = int(sys.argv[1])
+    msg = {"pin":17, "value":value}
+    ret = client.publish("topic/gpio", json.dumps(msg), qos=2, retain=False);
+    print (ret)
+
+    #ret.wait_for_publish():
+
+    if ret.is_published():
+        print ("is published")
+    
+    '''
+    try:
+        client.loop()
+    except KeyboardInterrupt:
+        client.disconnect()
+    '''
+    client.loop_stop()
+    client.disconnect()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print ('input 0 to turn off, 1 to turn on.')
+        sys.exit()
+    
+    main()
--- pi/subscriber_1.py
+++ pi/subscriber_1.py
@@ -4,12 +4,10 @@ import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import json
   
-# BCM GPIO编号
+# BCM GPIO
 pins = [17,18,27,22,23,24,25,4]
 def gpio_setup():
-    # 采用BCM编号
     GPIO.setmode(GPIO.BCM)
-    # 设置所有GPIO为输出状态，且输出低电平
     for pin in pins:
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, GPIO.LOW)
@@ -19,16 +17,12 @@ def gpio_destroy():
         GPIO.output(pin, GPIO.LOW)
         GPIO.setup(pin, GPIO.IN)
          
-# 连接成功回调函数
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    # 连接完成之后订阅gpio主题
     client.subscribe("topic/gpio")
   
-# 消息推送回调函数
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload.decode()))
-    # 获得负载中的pin 和 value
     gpio = json.loads(str(msg.payload.decode()))
     print (gpio)
   
@@ -41,13 +35,12 @@ def on_message(client, userdata, msg):
             print ('Turned on!')
   
 if __name__ == '__main__':
-    client = mqtt.Client()
+    client = mqtt.Client('id-sub-0001')
     client.on_connect = on_connect
     client.on_message = on_message
     gpio_setup()
      
     try:
-        # 请根据实际情况改变MQTT代理服务器的IP地址
         client.connect("111.111.111.1", 1883, 60)
         client.loop_forever()
     except KeyboardInterrupt:
