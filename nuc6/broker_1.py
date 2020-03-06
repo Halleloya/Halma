f04ba7ff59e8085c56fcf98448196c9ce912c21e
Halleloya <haoluoyao@163.com>
2020-03-06 17:35:28 -0500

    add hbmqttt

--- nuc6/broker_1.py
+++ nuc6/broker_1.py
@@ -8,6 +8,11 @@ from multiprocessing import Process
 
 
 logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+'''
+handler = logging.FileHandler("log.txt")
+handler.setLevel(logging.INFO)
+'''
 
 with open("config.yml", 'r') as cfgfile:
     config = yaml.load(cfgfile, Loader=yaml.FullLoader)
@@ -17,24 +22,32 @@ def broker_coro():
     broker = Broker(config)
     yield from broker.start()
 
+@asyncio.coroutine
+def broker_lisn():
+    while True:
+        print ('Lisn Function', logger)
+        print ('222222222222222222222222222222')
+        break # will do somthing...
+
 def on_connect(client, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
-  client.subscribe("topic/gpio") #topic/test")
+    print("Connected with result code "+str(rc))
+    client.subscribe("topic/gpio") #topic/test")
 
 def on_message(client, userdata, msg):
-  if msg.payload.decode() != None:
-    rcv_msg = msg.payload.decode()
-    print ('received:'+rcv_msg)
-    client_trans = mqtt.Client()
-    client_trans.connect("111.111.111.1", 1883, 60)
-    client_trans.publish("topic/gpio", rcv_msg);
-    client_trans.disconnect();
-    #client.disconnect()
+    if msg.payload.decode() != None:
+        rcv_msg = msg.payload.decode()
+        print ('received:'+rcv_msg)
+        client_trans = mqtt.Client()
+        client_trans.connect("111.111.111.1", 1883, 60)
+        client_trans.publish("topic/gpio", rcv_msg);
+        client_trans.disconnect();
+        #client.disconnect()
     
 def start_broker():
     formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=formatter)
+    logging.basicConfig(level=logging.INFO, format=formatter)
     asyncio.get_event_loop().run_until_complete(broker_coro())
+    #asyncio.get_event_loop().run_until_complete(broker_lisn())
     asyncio.get_event_loop().run_forever()
 
 def start_client():
@@ -48,10 +61,11 @@ def start_client():
     except KeyboardInterrupt:
         client.disconnect()
 
+def main():
+    Process (target = start_broker).start()
+    Process (target = start_client).start()
+
+    Process (target = broker_lisn).start()
 
 if __name__ == '__main__':
-    Process (target = start_broker).start()
-    print ("66666666666666666666666666666666666666") 
-    Process (target = start_client).start()
-    print ("77777777777777777777777777777777777777")
-    
+    main() 
